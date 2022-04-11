d6b8382d5a5bb95d97a325f85293ee1f954e852d
Aqib-1998 <aqib199800@gmail.com>
2022-04-11 20:07:38 +0500

    data bug solved

--- lib/app/modules/pages/views/measurement_page.dart
+++ lib/app/modules/pages/views/measurement_page.dart
@@ -70,90 +70,100 @@ class _MeasurementPageState extends State<MeasurementPage> {
                         .ref()
                         .child(widget.deviceId)
                         .update({'Case': 0}).whenComplete(() async {
-                      await FirebaseDatabase.instance
-                          .ref()
-                          .child(widget.deviceId)
-                          .once()
-                          .then((value) {
-                        values = value.snapshot.value! as Map<dynamic, dynamic>;
-                        temperature = "${values["Temperature"]}";
-                        spo2 = "${values["SPO2"]} %";
-                        heartRate = "${values["BPM"]} BPM";
-                        measure.value = false;
-                      });
-                    }).whenComplete(() {
-                      History history = History(
-                          temperature: temperature,
-                          spo2: spo2,
-                          bpm: heartRate,
-                          dateTime: DateTime.now(),
-                          id: "asdasdasd");
-                      FirebaseFirestore.instance
-                          .collection("Users")
-                          .doc(widget.uid)
-                          .collection("History")
-                          .add(history.toMap())
-                          .then((value) {
-                        FirebaseFirestore.instance
-                            .collection("Users")
-                            .doc(widget.uid)
-                            .collection("History")
-                            .doc(value.id)
-                            .update({"id": value.id});
+                      3.seconds.delay().then((value) async {
+                        await FirebaseDatabase.instance
+                            .ref()
+                            .child(widget.deviceId)
+                            .once()
+                            .then((value) {
+                          values =
+                              value.snapshot.value! as Map<dynamic, dynamic>;
+                          temperature = "${values["Temperature"]}";
+                          spo2 = "${values["SPO2"]} %";
+                          heartRate = "${values["BPM"]} BPM";
+                        }).whenComplete(() {
+                          History history = History(
+                              temperature: temperature,
+                              spo2: spo2,
+                              bpm: heartRate,
+                              dateTime: DateTime.now(),
+                              id: "asdasdasd");
+
+                          FirebaseFirestore.instance
+                              .collection("Users")
+                              .doc(widget.uid)
+                              .collection("History")
+                              .add(history.toMap())
+                              .then((value) {
+                            FirebaseFirestore.instance
+                                .collection("Users")
+                                .doc(widget.uid)
+                                .collection("History")
+                                .doc(value.id)
+                                .update({"id": value.id});
+                            measure.value = false;
+
+                            Get.defaultDialog(
+                                barrierDismissible: false,
+                                title: "Result",
+                                titleStyle: kLargerTextStyle.copyWith(
+                                    color: kMainColor),
+                                content: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 15.w),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            MediumText(
+                                                text: "Heart Rate:",
+                                                textColor: kMainColor),
+                                            MediumText(
+                                                text: heartRate,
+                                                textColor: kMainColor)
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 20.h,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 15.w),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            MediumText(
+                                                text: "SPO2:",
+                                                textColor: kMainColor),
+                                            MediumText(
+                                                text: spo2,
+                                                textColor: kMainColor)
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 10.h,
+                                      ),
+                                      ElevatedButton(
+                                          onPressed: () => Get.back(),
+                                          child: SmallText(
+                                              text: "Back to Home",
+                                              textColor: kWhiteColor))
+                                    ],
+                                  ),
+                                ));
+                          });
+                        });
                       });
                     });
-
-                    Get.defaultDialog(
-                        barrierDismissible: false,
-                        title: "Result",
-                        titleStyle:
-                            kLargerTextStyle.copyWith(color: kMainColor),
-                        content: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 15.w),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    MediumText(
-                                        text: "Heart Rate:",
-                                        textColor: kMainColor),
-                                    MediumText(
-                                        text: heartRate, textColor: kMainColor)
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20.h,
-                              ),
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 15.w),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    MediumText(
-                                        text: "SPO2:", textColor: kMainColor),
-                                    MediumText(
-                                        text: spo2, textColor: kMainColor)
-                                  ],
-                                ),
-                              ),
-                              SizedBox(
-                                height: 10.h,
-                              ),
-                              ElevatedButton(
-                                  onPressed: () => Get.back(),
-                                  child: SmallText(
-                                      text: "Back to Home",
-                                      textColor: kWhiteColor))
-                            ],
-                          ),
-                        ));
                   });
                 })
               : Center(
